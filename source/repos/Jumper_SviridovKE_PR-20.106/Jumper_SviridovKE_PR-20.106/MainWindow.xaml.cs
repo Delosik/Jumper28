93f80a6d63e09590cc4cc4a95afef1dad3fa57a7
Свиридов Кирилл Евгеньевич <pr-20.107k-sk@NAVIATK.RU>
2023-02-28 14:46:29 +0700

    first commit

---
+++ source/repos/Jumper_SviridovKE_PR-20.106/Jumper_SviridovKE_PR-20.106/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Jumper_SviridovKE_PR_20._106.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jumper_SviridovKE_PR_20._106
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public class helper
+    {
+        public static bool flag = false;
+        public static int prioritet = 0;
+
+        public static JumperSviridovEntities1 ent;
+        public static JumperSviridovEntities1 GetContext()
+        {
+            if (ent == null)
+            {
+                ent = new JumperSviridovEntities1();
+            }
+            return ent;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Content = new Pages.Page1(mainFrame);
+
+        }
+
+        public object frame { get; private set; }
+
+        private void frame_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            try
+            {
+                Pages.Page1 pg = (Pages.Page1)e.Content;
+                pg.Load();
+            }
+            catch { };
+
+        }
+
+    }
+}
---
+++ source/repos/Jumper_SviridovKE_PR-20.106/Jumper_SviridovKE_PR-20.106/Model/Connect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumper_SviridovKE_PR_20._106.Model
+{
+    class Connect
+    {
+        private static JumperSviridovEntities1 jumpcon = new JumperSviridovEntities1();
+
+        public static JumperSviridovEntities1 GetContext()
+        {
+            if (jumpcon == null)
+            {
+                jumpcon = new JumperSviridovEntities1();
+            } 
+            return jumpcon;
+                
+        }
+    }
+}
